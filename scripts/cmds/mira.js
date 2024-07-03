770ba422ffa1e1b8fa386997b31b40e02913648a
TOWHID-AHMED-153 <163658299+TOWHID-AHMED-153@users.noreply.github.com>
2024-07-04 02:46:34 +0600

    Update mira.js

--- scripts/cmds/mira.js
+++ scripts/cmds/mira.js
@@ -1,58 +1,56 @@
 const axios = require("axios");
 
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+  );
+  return base.data.api;
+};
+
 module.exports.config = {
-  name: "bby",
+  name: "mira",
   version: "1.0.0",
-  role: 0, 
-  author: "dipto", 
-  description: "better then all Sim simi with multiple conversation",
-  usePrefix: true,
-  guide: "[message]",
+  role: 0,
+  author: "dipto",
+  description: "better than all Sim simi with multiple conversation",
+  guide: { en: "[message]" },
   category: "ChatBots",
   coolDowns: 5,
 };
-module.exports.onReply = async function ({ api, event}) {
-  if (event.type == "message_reply") {
-  const reply = event.body.toLowerCase();;
-  if (isNaN(reply)) {
-    const response = await axios.get(`${global.GoatBot.config.api}/baby?text=${encodeURIComponent(reply)}`)
-    const ok = response.data.reply;
-    if(response.data.react){
-      api.setMessageReaction(response.data.react, event.messageID, (err) => {}, true);
-    }
-    await api.sendMessage(ok ,event.threadID,(error, info) => {
-  global.GoatBot.onReply.set(info.messageID,{
-    commandName: this.config.name,
-    type: 'reply',
-    messageID: info.messageID,
-    author: event.senderID,
-    link: ok
-  })},event.messageID)
-  }
-  }
-}
-module.exports.onStart = async function ({ api, args, event }) {
+
+module.exports.onStart = async function ({ api, args, event }) { };
+module.exports.onChat = async function ({ api, args, event }) {
   try {
-    const dipto = args.join(" ").toLowerCase();
-    if (!args[0]) {
-      api.sendMessage(
-        "Please provide a question to answer\n\nExample:\nbaby ki koro",
-  event.threadID,  event.messageID ); return;}
-    if (dipto) {
-      const response = await axios.get(`https://nobs-api.onrender.com/dipto/baby?text=${dipto}`);
-         const mg = response.data.reply;
-      if(response.data.react){
-        api.setMessageReaction(response.data.react, event.messageID, (err) => {}, true);
+    if (event.body) {
+      const cm = ["baby", "bby"];
+      const cml = event.body.toLowerCase();
+      const dipto = args.join(" ");
+      if (!args[0]) {
+        api.sendMessage(
+          "Please provide a question to answer\n\nExample:\nbaby ki koro",
+          event.threadID,
+          event.messageID
+        );
+        return;
       }
-      await api.sendMessage({body: mg ,},event.threadID,(error, info) => {
-  global.GoatBot.onReply.set(info.messageID,{
-        commandName: this.config.name,
-    type: 'reply',
-    messageID: info.messageID,
-    author: event.senderID,
-    link: mg
-  })},event.messageID);
+      if (dipto) {
+        const response = await axios.get(
+          `${await baseApiUrl()}/baby?text=${dipto}`
+        );
+        const mg = response.data.reply;
+        await api.sendMessage(
+          { body: mg },
+          event.threadID,
+          event.messageID
+        );
+      }
     }
-  } catch (error) {console.error(`Failed to get an answer: ${error.message}`);
-api.sendMessage(`${error.message}.\nAn error`,event.threadID,event.messageID);}
+  } catch (error) {
+    console.error(`Failed to get an answer: ${error.message}`);
+    api.sendMessage(
+      `${error.message}.\nAn error occurred.`,
+      event.threadID,
+      event.messageID
+    );
+  }
 };
